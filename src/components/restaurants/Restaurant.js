626eeba6ced13df21dad4346feb11bc246c1c131
Dusch4593 <brandondusch@gmail.com>
2020-07-23 08:38:16 -0400

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import ReviewsContainer from '../../containers/ReviewsContainer'
 
+
 class Restaurant extends Component {
 
 
   render() {
-    const { restaurant } = this.props;
+
 
     return (
       <div>
         <li>
-          {restaurant.text}
-          <button> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
+          {this.props.restaurant.text}
+          <button onClick={() => this.props.delete(this.props.restaurant.id)}> DELETE </button> <br />
+          < ReviewsContainer restaurant={this.props.restaurant}/>
         </li>
       </div>
     );
   }
 };
 
-export default Restaurant;
+export default Restaurant
